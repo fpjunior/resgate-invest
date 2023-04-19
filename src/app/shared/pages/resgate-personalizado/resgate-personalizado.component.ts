1f07612f087c9eb0e7968894a37aaf2e4bee2f54
fernando.junior <fernando.junior@informata.com.br>
2023-04-18 23:59:15 -0300

    :wrench: cria tela de resgate

---
+++ src/app/shared/pages/resgate-personalizado/resgate-personalizado.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-resgate-personalizado',
+  templateUrl: './resgate-personalizado.component.html',
+  styleUrls: ['./resgate-personalizado.component.css']
+})
+export class ResgatePersonalizadoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
