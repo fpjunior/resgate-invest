782407da7e90c32de153be5bb0cf957286df2ab2
fernando.junior <fernando.junior@informata.com.br>
2023-04-19 18:05:08 -0300

    :sparkles:ajusta funcionalidade de regras de resgates

--- src/app/shared/pages/resgate-personalizado/resgate-personalizado.component.ts
+++ src/app/shared/pages/resgate-personalizado/resgate-personalizado.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 import { InvestimentoService } from '../lista-de-investimento/service/investimento.service';
 import { InvestimentoModel } from '../lista-de-investimento/model/investimento';
 
@@ -10,19 +10,31 @@ import { InvestimentoModel } from '../lista-de-investimento/model/investimento';
 })
 export class ResgatePersonalizadoComponent implements OnInit {
 
-  investimentos!: any[];
+  @ViewChild('btnSalvar') btnSalvar!: ElementRef;
+
+  investimentos!: any;
   nomeInvestimento!: string;
   nome!: string;
   objetivo!: string;
   saldoTotal!: string;
   dadosPreencherTabela!: any
-  valorResgate!: string
-
-  constructor(private route: ActivatedRoute, private investimentoService: InvestimentoService,) {
+  valorResgate!: string;
+  showModalError: boolean = false;
+  showModalSuccess: boolean = false;
+  contentMessage: string = '';
+  modalTitle: string = 'Resgate Efetuado com sucesso!';
+  msgError = ''
+  labelBtnModal = 'NOVO RESGATE'
+  disableBtn: boolean = true
+  mensagemResgate: string = '';
+  constructor(
+    private route: ActivatedRoute,
+    private investimentoService: InvestimentoService,
+    private router: Router
+  ) {
     this.route.queryParams.subscribe(params => {
       this.nome = params['nomeInvestimento'];
     });
-
   }
 
   ngOnInit() {
@@ -35,6 +47,8 @@ export class ResgatePersonalizadoComponent implements OnInit {
         this.investimentos = data.response.data?.listaInvestimentos.filter((e: any) => {
           return e.nome === this.nome
         })
+        this.objetivo = this.investimentos[0].objetivo
+        this.saldoTotal = this.investimentos[0].saldoTotal
         this.dadosPreencherTabela = this.investimentos[0].acoes
       },
       (error) => {
@@ -42,19 +56,52 @@ export class ResgatePersonalizadoComponent implements OnInit {
     );
   }
 
-  somarResgate(event: any) {
-    let totalResgate = 0;
-    for (let investimento of this.dadosPreencherTabela) {
-        if (investimento.valorResgate) {
-            let valor = investimento.valorResgate.toString().replace(/\D/g, '');
-            if (valor !== '') {
-              totalResgate += parseFloat(valor)/100;
-            }
+  verificarCondicoesInvestimentos(): string | null {
+    let mensagemResgate = '';
+    for (let investimento of this.investimentos[0].acoes) {
+      let vPercentual = Math.trunc(investimento.percentual * 100);
+      let vResgate = parseFloat(investimento.valorResgate?.replace(/[^0-9.-]+/g, ''));
+      if(vResgate){
+        if (vResgate > vPercentual) {
+          mensagemResgate += `${investimento.nome}: O valor a resgatar não pode ser maior que ${this.formatCurrency(vPercentual)}\n`;
         }
+      }
+
     }
-    console.log("Valor total do resgate: R$ " + totalResgate.toLocaleString('pt-BR', {minimumFractionDigits: 2}));
-    this.valorResgate = "R$ " + totalResgate.toLocaleString('pt-BR', {minimumFractionDigits: 2});
-    return this.valorResgate
-}
+    return mensagemResgate !== '' ? mensagemResgate : null;
+  }
+
+  somarResgate() {
+    let mensagemErro = this.verificarCondicoesInvestimentos();
+    if (mensagemErro) {
+      // Exibir modal de erro
+      this.showModalError = true;
+      this.mensagemResgate = mensagemErro;
+    } else {
+      // Realizar o resgate e exibir modal de sucesso
+      // ...
+      this.showModalSuccess = true;
+    }
+  }
+
+  returnList() {
+      this.router.navigate(['/lista-de-investimento']);
+  }
+
+  private formatCurrency(value: number) {
+    return (value / 100).toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+  }
+
+  getInfoInput(event: string, rowValue: { percentual: number, nome: string }) {
+    const ALLOWED_VALUES = ['R$', '', 'R$ 0,0'];
+    const allowedValue = ALLOWED_VALUES.includes(event);
+    const value = parseFloat(event.replace(/[^0-9.-]+/g, ''));
+    let newValue = Math.trunc(rowValue.percentual * 100);
+    if (value > newValue) {
+      this.contentMessage = 'Você preencheu um ou mais campos com um valor acima do permitido:'
+      this.disableBtn = true;
+    }
+    this.disableBtn = allowedValue ? true : false;
+  }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -13,6 +13,7 @@ import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { RealBrasilMaskDirective } from './util/regex-money.directive';
+import { DialogModule } from 'primeng/dialog';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
@@ -32,6 +33,7 @@ const routes: Routes = [
     InputTextModule,
     ButtonModule,
     InputNumberModule,
+    DialogModule
   ],
   exports: [
     FormsModule,
